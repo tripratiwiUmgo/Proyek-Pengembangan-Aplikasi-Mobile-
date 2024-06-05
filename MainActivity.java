6a62d528df43431b1f61a29d17123505eac4bede
tripratiwiUmgo <tripratiwi@umgo.ac.id>
2024-06-05 08:50:46 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,1 +1,29 @@
+package com.example.taskmanager;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private TaskAdapter taskAdapter;
+    private TaskRepository taskRepository;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        taskRepository = new TaskRepository();
+        List<Task> tasks = taskRepository.getAllTasks();
+        taskAdapter = new TaskAdapter(tasks);
+        recyclerView.setAdapter(taskAdapter);
+    }
+}
 
