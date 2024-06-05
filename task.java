aa1e9fd7d007847acb213baeaead869c2e1165cd
tripratiwiUmgo <tripratiwi@umgo.ac.id>
2024-06-05 08:51:27 +0800

    Update task.java

--- task.java
+++ task.java
@@ -1,1 +1,20 @@
+package com.example.taskmanager;
+
+public class Task {
+    private String title;
+    private String description;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
 
